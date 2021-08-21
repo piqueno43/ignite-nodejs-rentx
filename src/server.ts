59cd89de6c6f768952e3a2a8a70eb4a9212b9c98
Edivaldo Silva <piqueno43@gmail.com>
2021-08-21 08:16:05 -0300

    Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hello!",
-  });
-});
+app.use(categoriesRoutes);
 
-app.post("/courses", (request, response) => {
-  const name = request.body;
-
-  return response.json({ name });
-});
 app.listen(3333, () => console.log("Server is running!"));
