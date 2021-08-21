e79e56c40c50f48d22f43281d174238f2731e9dc
Edivaldo Silva <piqueno43@gmail.com>
2021-08-21 09:38:12 -0300

    Corrigindo exportaçoes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { CategoryRepository } from "repositories/CategoriesRepository";
-import { CreateCategoryService } from "services/CreateCategoryService";
+
+import { CategoryRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 
