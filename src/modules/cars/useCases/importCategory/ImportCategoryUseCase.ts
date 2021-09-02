686d876f28c4c6cb4ac6dd5c4d728376e00262a4
Edivaldo Silva <piqueno43@gmail.com>
2021-09-01 23:02:58 -0300

    Removendo os arquivos de upload

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -29,7 +29,7 @@ class ImportCategoryUseCase {
           });
         })
         .on("end", () => {
-          // fs.promises.unlink(file.path);
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
