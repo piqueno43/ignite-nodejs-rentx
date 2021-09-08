bbbe75d2577e3451bd7e5df666fcb83c2e51d3f8
Edivaldo Silva <piqueno43@gmail.com>
2021-09-08 10:38:53 -0300

    Criptografrar senha

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -11,7 +11,6 @@ class CreateUserController {
 
     await createUserUseCase.execute({
       name,
-      username,
       email,
       password,
       driver_license,
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash, hashSync } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -11,16 +12,22 @@ class CreateUserUseCase {
   ) {}
   async execute({
     name,
-    username,
     email,
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      username,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
