55d4c2cd9fbef3837beda88b97e23fa1b2c06fda
Edivaldo Silva <piqueno43@gmail.com>
2021-09-15 05:22:00 -0300

    Finalizando testes unitarios

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -4,19 +4,24 @@ interface IOptions {
   host: string;
 }
 
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = "database"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-  createConnection({
-    ...options,
-  });
-});
+// getConnectionOptions().then((options) => {
+//   const newOptions = options as IOptions;
+//   newOptions.host = "database_ignite"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//   createConnection({
+//     ...options,
+//   });
+// });
 
-export default async (host = "database"): Promise<Connection> => {
+export default async (host = "database_ignite"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "rentx_test"
+          : defaultOptions.database,
     })
   );
 };
