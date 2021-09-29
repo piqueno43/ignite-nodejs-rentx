9af8033f692f46c1d919775551cb1f8f7db41dd9
Edivaldo Silva <piqueno43@gmail.com>
2021-09-29 12:43:06 -0300

    Database build

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,23 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (): Promise<Connection> => {
+interface IOptions {
+  host: string;
+}
+
+// getConnectionOptions().then((options) => {
+//   const newOptions = options as IOptions;
+//   newOptions.host = "database_ignite"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//   createConnection({
+//     ...options,
+//   });
+// });
+
+export default async (host = "database_ignite"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
